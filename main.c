c54279ba9be8a307c03b562c3757ddde802f1150
Jared Meit <jared.meit@gmail.com>
2021-07-08 05:35:58 +0100

    isolate ws2811 to lib

--- main.c
+++ main.c
@@ -53,11 +53,6 @@ static char VERSION[] = "XX.YY.ZZ";
 
 #include "ws2811.h"
 
-#include "touchscreen.h"
-#include "colours.h"
-#include "mqtt.h"
-
-
 #define ARRAY_SIZE(stuff)       (sizeof(stuff) / sizeof(stuff[0]))
 
 // defaults for cmdline options
@@ -68,22 +63,17 @@ static char VERSION[] = "XX.YY.ZZ";
 #define STRIP_TYPE              WS2811_STRIP_GRB		// WS2812/SK6812RGB integrated chip+leds
 //#define STRIP_TYPE            SK6812_STRIP_RGBW		// SK6812RGBW (NOT SK6812RGB)
 
-#define PANELS                  4
-#define PANEL_W                 32
-#define PANEL_H                 8
-#define WIDTH                   PANEL_W
-#define HEIGHT                  ( PANEL_H * PANELS )
-#define LED_COUNT               ( WIDTH * HEIGHT )
 
-unsigned int width = WIDTH;
-unsigned int height = HEIGHT;
-unsigned int led_count = LED_COUNT;
-unsigned int hitbox_w, hitbox_h,
+unsigned int num_panels,
+    led_count,
+    width, height,
+    panel_w, panel_h,
     old_x, old_y;
-uint32_t color = 0x0020A0D2;
 unsigned int pixel = 0;
 
-int clear_on_exit = 0;
+int clear_on_exit = 1;
+
+ws2811_led_t *matrix;
 
 ws2811_t ledstring =
 {
@@ -109,9 +99,6 @@ ws2811_t ledstring =
     },
 };
 
-ws2811_led_t *matrix;
-
-static uint8_t running = 1;
 
 void matrix_render(void)
 {
@@ -136,35 +123,6 @@ void matrix_clear(void)
         {
             matrix[y * width + x] = 0;
         }
-    }
-}
-
-unsigned int dotspos[] = { 0, 1, 2, 3, 4, 5, 6, 7 };
-ws2811_led_t dotcolors[] =
-{
-    0x00FF0000,  // red
-    0x00201000,  // orange
-    0x00202000,  // yellow
-    0x00002000,  // green
-    0x00002020,  // lightblue
-    0x00000020,  // blue
-    0x00100010,  // purple
-    0x00200010,  // pink
-};
-
-void rainbow_test(void)
-{
-    int i;
-
-    for (i = 0; i < (int)(ARRAY_SIZE(dotspos)); i++)
-    {
-        dotspos[i]++;
-        if (dotspos[i] > (width - 1))
-        {
-            dotspos[i] = 0;
-        }
-
-        matrix[dotspos[i] + (height - 1) * width] = dotcolors[i];
     }
 }
 
@@ -218,230 +176,15 @@ void drawPixels()
 }
 
 
-void draw_to_matrix( unsigned int** crds )
-{
-    unsigned int x_raw= *crds[0];
-    unsigned int y_raw= *crds[1];
-    unsigned int x = fmin( floor( x_raw / hitbox_w ), PANEL_W-1 );
-    unsigned int y = floor( y_raw / hitbox_h );
-    char color_hexstr[15];
-    sprintf( color_hexstr, "0x%08X", color );
-
-    if ( x_raw > getTouchscreenWidth() )
-    {
-        unsigned int tmp = y_raw * ( getTouchscreenHeight() - 1 );
-        uint32_t new_color = getColor( tmp );
-        if ( new_color == color ){ return; }
-        color = new_color;
-        drawPixels();
-        printf( "color: %s\n", color_hexstr );
-        return;
-    }
-
-    // Debounce
-    if( x == old_x && y == old_y ){ return; }
-    old_x = x;
-    old_y = y;
-
-    pixel = coords_to_pixel( x, y );
-    drawPixels();
-    char message[25];
-    sprintf( message, "%i,%i %s", x,y,color_hexstr );
-    mqtt_publish( message );
-}
-
-
-static void ctrl_c_handler(int signum)
-{
-	(void)(signum);
-    running = 0;
-}
-
-static void setup_handlers(void)
-{
-    struct sigaction sa =
-    {
-        .sa_handler = ctrl_c_handler,
-    };
-
-    sigaction(SIGINT, &sa, NULL);
-    sigaction(SIGTERM, &sa, NULL);
-}
-
-
-void parseargs(int argc, char **argv, ws2811_t *ws2811)
-{
-	int index;
-	int c;
-
-	static struct option longopts[] =
-	{
-		{"help", no_argument, 0, 'h'},
-		{"dma", required_argument, 0, 'd'},
-		{"gpio", required_argument, 0, 'g'},
-		{"invert", no_argument, 0, 'i'},
-		{"clear", no_argument, 0, 'c'},
-		{"strip", required_argument, 0, 's'},
-		{"height", required_argument, 0, 'y'},
-		{"width", required_argument, 0, 'x'},
-		{"version", no_argument, 0, 'v'},
-		{0, 0, 0, 0}
-	};
-
-	while (1)
-	{
-
-		index = 0;
-		c = getopt_long(argc, argv, "cd:g:his:vx:y:", longopts, &index);
-
-		if (c == -1)
-			break;
-
-		switch (c)
-		{
-		case 0:
-			/* handle flag options (array's 3rd field non-0) */
-			break;
-
-		case 'h':
-			fprintf(stderr, "%s version %s\n", argv[0], VERSION);
-			fprintf(stderr, "Usage: %s \n"
-				"-h (--help)    - this information\n"
-				"-s (--strip)   - strip type - rgb, grb, gbr, rgbw\n"
-				"-x (--width)   - matrix width (default 8)\n"
-				"-y (--height)  - matrix height (default 8)\n"
-				"-d (--dma)     - dma channel to use (default 10)\n"
-				"-g (--gpio)    - GPIO to use\n"
-				"                 If omitted, default is 18 (PWM0)\n"
-				"-i (--invert)  - invert pin output (pulse LOW)\n"
-				"-c (--clear)   - clear matrix on exit.\n"
-				"-v (--version) - version information\n"
-				, argv[0]);
-			exit(-1);
-
-		case 'D':
-			break;
-
-		case 'g':
-			if (optarg) {
-				int gpio = atoi(optarg);
-/*
-	PWM0, which can be set to use GPIOs 12, 18, 40, and 52.
-	Only 12 (pin 32) and 18 (pin 12) are available on the B+/2B/3B
-	PWM1 which can be set to use GPIOs 13, 19, 41, 45 and 53.
-	Only 13 is available on the B+/2B/PiZero/3B, on pin 33
-	PCM_DOUT, which can be set to use GPIOs 21 and 31.
-	Only 21 is available on the B+/2B/PiZero/3B, on pin 40.
-	SPI0-MOSI is available on GPIOs 10 and 38.
-	Only GPIO 10 is available on all models.
-
-	The library checks if the specified gpio is available
-	on the specific model (from model B rev 1 till 3B)
-
-*/
-				ws2811->channel[0].gpionum = gpio;
-			}
-			break;
-
-		case 'i':
-			ws2811->channel[0].invert=1;
-			break;
-
-		case 'c':
-			clear_on_exit=1;
-			break;
-
-		case 'd':
-			if (optarg) {
-				int dma = atoi(optarg);
-				if (dma < 14) {
-					ws2811->dmanum = dma;
-				} else {
-					printf ("invalid dma %d\n", dma);
-					exit (-1);
-				}
-			}
-			break;
-
-		case 'y':
-			if (optarg) {
-				height = atoi(optarg);
-				if (height > 0) {
-					ws2811->channel[0].count = height * width;
-				} else {
-					printf ("invalid height %d\n", height);
-					exit (-1);
-				}
-			}
-			break;
-
-		case 'x':
-			if (optarg) {
-				width = atoi(optarg);
-				if (width > 0) {
-					ws2811->channel[0].count = height * width;
-				} else {
-					printf ("invalid width %d\n", width);
-					exit (-1);
-				}
-			}
-			break;
-
-		case 's':
-			if (optarg) {
-				if (!strncasecmp("rgb", optarg, 4)) {
-					ws2811->channel[0].strip_type = WS2811_STRIP_RGB;
-				}
-				else if (!strncasecmp("rbg", optarg, 4)) {
-					ws2811->channel[0].strip_type = WS2811_STRIP_RBG;
-				}
-				else if (!strncasecmp("grb", optarg, 4)) {
-					ws2811->channel[0].strip_type = WS2811_STRIP_GRB;
-				}
-				else if (!strncasecmp("gbr", optarg, 4)) {
-					ws2811->channel[0].strip_type = WS2811_STRIP_GBR;
-				}
-				else if (!strncasecmp("brg", optarg, 4)) {
-					ws2811->channel[0].strip_type = WS2811_STRIP_BRG;
-				}
-				else if (!strncasecmp("bgr", optarg, 4)) {
-					ws2811->channel[0].strip_type = WS2811_STRIP_BGR;
-				}
-				else if (!strncasecmp("rgbw", optarg, 4)) {
-					ws2811->channel[0].strip_type = SK6812_STRIP_RGBW;
-				}
-				else if (!strncasecmp("grbw", optarg, 4)) {
-					ws2811->channel[0].strip_type = SK6812_STRIP_GRBW;
-				}
-				else {
-					printf ("invalid strip %s\n", optarg);
-					exit (-1);
-				}
-			}
-			break;
-
-		case 'v':
-			fprintf(stderr, "%s version %s\n", argv[0], VERSION);
-			exit(-1);
-
-		case '?':
-			/* getopt_long already reported error? */
-			exit(-1);
-
-		default:
-			exit(-1);
-		}
-	}
-}
-
-
-int main(int argc, char *argv[])
+int ws2811_init( int _num_panels, int _panel_w, int _panel_h )
 {
     ws2811_return_t ret;
-
-    sprintf(VERSION, "%d.%d.%d", VERSION_MAJOR, VERSION_MINOR, VERSION_MICRO);
-
-    parseargs(argc, argv, &ledstring);
+    panels = _num_panels;
+    panel_w = _panel_w;
+    panel_h = _panel_h;
+    width = panel_w;
+    height = panel_h * num_panels;
+    led_count = width * height;
 
     matrix = malloc(sizeof(ws2811_led_t) * width * height);
 
@@ -453,38 +196,16 @@ int main(int argc, char *argv[])
         return ret;
     }
 
-    hitbox_w = getTouchscreenWidth() / WIDTH;
-    hitbox_h = getTouchscreenHeight() / HEIGHT;
+    return ret;
+}
 
-    mqtt_init();
-
-    startTouchscreenRead( &running, draw_to_matrix );
-/*
-    while (running)
-    {
-        matrix_raise();
-        matrix_bottom();
-        matrix_render();
-
-        if ((ret = ws2811_render(&ledstring)) != WS2811_SUCCESS)
-        {
-            fprintf(stderr, "ws2811_render failed: %s\n", ws2811_get_return_t_str(ret));
-            break;
-        }
-
-        // 15 frames /sec
-        usleep(1000000 / 15);
-    }
-*/
-    if (clear_on_exit) {
-        matrix_clear();
-        matrix_render();
-        ws2811_render(&ledstring);
-    }
+int ws2811_destroy()
+{
+    matrix_clear();
+    matrix_render();
+    ws2811_render(&ledstring);
 
     ws2811_fini(&ledstring);
-    mqtt_destroy();
 
-    printf ("\n");
-    return ret;
+    return 0;
 }
